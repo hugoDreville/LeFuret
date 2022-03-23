4cb43a6d9a72a53a1256b397430cd242042d3c4a
hugo <hdreville@gmail.com>
2022-03-23 13:51:28 +0100

    init

---
+++ dart_le_furet/bin/dart_le_furet.dart
@@ -0,0 +1,5 @@
+import 'package:dart_le_furet/dart_le_furet.dart' as dart_le_furet;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_le_furet.calculate()}!');
+}
---
+++ dart_le_furet/test/dart_le_furet_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_le_furet/dart_le_furet.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
